4bf535e88f7a9c6735adeede2c18f14ec02bc146
ryshiba <race.lifes.a.race@gmail.com>
2020-05-11 17:56:43 +0900

    [add]delete_whitespace

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,11 @@
 class Tag < ApplicationRecord
-  validates :name, format: { without: /[^ 　]+/ }
+  before_validation :delete_whitespace
 
   has_many :post_tag_relations, dependent: :destroy
   has_many :posts, through: :post_tag_relations, dependent: :destroy
+
+  private
+    def delete_whitespace
+      self.name = name.strip
+    end
 end
